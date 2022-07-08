d105714a598f1cc1348d710c0511c9e2b68fc622
tim.li <tim.li@laxtongroup.com>
2022-07-08 14:17:54 +0800

    feat 新增 npm-rollup-ts 模板

---
+++ packages/npm-rollup-ts-template/template/__test__/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('core', () => {
+  it('should work', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ packages/npm-rollup-ts-template/template/rollup.config.js
@@ -0,0 +1,80 @@
+import esbuild from 'rollup-plugin-esbuild'// 将 ts 转换成 js
+import dts from 'rollup-plugin-dts' // 生成 dts
+import resolve from '@rollup/plugin-node-resolve' // 让rollup支持nodejs的模块解析机制
+import commonjs from '@rollup/plugin-commonjs' // 将CommonJs模块转换为es6
+import json from '@rollup/plugin-json' // 将json文件转换为es6
+import { defineConfig } from 'rollup'
+import fse from 'fs-extra'
+import pkg from './package.json'
+
+const plugins = [
+  resolve({
+    preferBuiltins: true,
+  }),
+  json(),
+  commonjs(),
+  esbuild({
+    target: 'node14',
+  }),
+]
+
+function onwarn(message) {
+  if (message.code === 'EMPTY_BUNDLE')
+    return
+  console.error(message)
+}
+
+const { createEntries } = require('./utils')
+const files = fse.readdirSync('src')
+
+const entries = {}
+
+files.forEach((file) => {
+  createEntries(file, __dirname, 'src', entries)
+})
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+  'child_process',
+]
+
+export default defineConfig([
+  {
+    input: entries,
+    output: {
+      dir: 'lib',
+      format: 'esm',
+      entryFileNames: '[name].mjs',
+      chunkFileNames: 'chunk-[name].mjs',
+    },
+    external,
+    plugins,
+    onwarn,
+  },
+  {
+    input: entries,
+    output: {
+      dir: 'lib',
+      format: 'cjs',
+      entryFileNames: '[name].cjs',
+      chunkFileNames: 'chunk-[name].cjs',
+    },
+    external,
+    plugins,
+    onwarn,
+  },
+  {
+    input: entries,
+    output: {
+      dir: 'lib',
+      entryFileNames: '[name].d.ts',
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      dts({ respectExternal: true }),
+    ],
+    onwarn,
+  },
+])
